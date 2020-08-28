c048cde283a5eb97266ab5cec00082e7c704156c
Nicole Casares <nicolecasares@gmail.com>
2020-08-28 07:50:27 -0700

    added Quotes

--- js/script.js
+++ js/script.js
@@ -13,23 +13,25 @@ project 1 - A Random Quote Generator
 
 let quotes = [
   {
-    'name':  'Reshma Saujani, Founder of Girls Who Code',
+    'source':  'Reshma Saujani, Founder of Girls Who Code',
     'quote': 'I also say to my team: Do 10% of your job shittily. Its okay to do something shittily. Perfectionism prevents us from taking double steps in our career. We think we have to be perfect, but we dont.',
+    'citation':'Brave Not Perfect',
+    'year': '2019',
   },
   {
-    'name': 'Sarah Friar, Chief Financial Officer at Square',
+    'source': 'Sarah Friar, Chief Financial Officer at Square',
     'quote': 'A diversity of thought, perspective and culture is important in any field, not just engineering. Surround yourself with people who support you and get involved in [coding] programs.',
   },
   {
-    'name': 'Vanessa Hurst, Co-Founder of Girl Develop It',
+    'source': 'Vanessa Hurst, Co-Founder of Girl Develop It',
     'quote': 'Feeling a little uncomfortable with your skills is a sign of learning, and continuous learning is what the tech industry thrives on! It’s important to seek out environments where you are supported, but where you have the chance to be uncomfortable and learn new things.',
   },
   {
-    'name': 'Amber Gordon, Founder of Femsplain',
+    'source': 'Amber Gordon, Founder of Femsplain',
     'quote': 'Trust in yourself, believe that your voice matters, and know that your words are good enough.',
   },
   {
-    'name': 'Ara Katz, Chief Marketing Officer at Spring:',
+    'source': 'Ara Katz, Chief Marketing Officer at Spring:',
     'quote': 'There is no recipe, there is no one way to do things — there is only your way. And if you can recognize that in yourself and accept and appreciate that in others, you can make magic.',
   },
 
@@ -41,6 +43,10 @@ let quotes = [
  * `getRandomQuote` function
 ***/
 
+let getRandomQuote = () => {
+  return "Hello World!";
+}
+console.log(getRandomQuote());
 
 
 /***
@@ -54,4 +60,4 @@ let quotes = [
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
