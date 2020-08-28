693c293c24d46090e8a7a3aab5af73dbf9b53b75
Nicole Casares <nicolecasares@gmail.com>
2020-08-27 22:25:17 -0700

    added Quotes

--- js/script.js
+++ js/script.js
@@ -11,6 +11,30 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+  {
+    'name':  'Reshma Saujani, Founder of Girls Who Code',
+    'quote': 'I also say to my team: Do 10% of your job shittily. Its okay to do something shittily. Perfectionism prevents us from taking double steps in our career. We think we have to be perfect, but we dont.',
+  },
+  {
+    'name': 'Sarah Friar, Chief Financial Officer at Square',
+    'quote': 'A diversity of thought, perspective and culture is important in any field, not just engineering. Surround yourself with people who support you and get involved in [coding] programs.',
+  },
+  {
+    'name': 'Vanessa Hurst, Co-Founder of Girl Develop It',
+    'quote': 'Feeling a little uncomfortable with your skills is a sign of learning, and continuous learning is what the tech industry thrives on! It’s important to seek out environments where you are supported, but where you have the chance to be uncomfortable and learn new things.',
+  },
+  {
+    'name': 'Amber Gordon, Founder of Femsplain',
+    'quote': 'Trust in yourself, believe that your voice matters, and know that your words are good enough.',
+  },
+  {
+    'name': 'Ara Katz, Chief Marketing Officer at Spring:',
+    'quote': 'There is no recipe, there is no one way to do things — there is only your way. And if you can recognize that in yourself and accept and appreciate that in others, you can make magic.',
+  },
+
+];
+
 
 
 /***
