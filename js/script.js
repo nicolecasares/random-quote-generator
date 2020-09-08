9f8521ff95b1c91749f67a5ff11c57fa20a2f0c7
Nicole Casares <nicolecasares@gmail.com>
2020-09-07 22:24:28 -0700

    addded comments to code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+GOAL: Exceeds Expectations Requirements
 ******************************************/
 
 /*** 
