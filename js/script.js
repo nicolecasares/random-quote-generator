64122e4ba826a31e8278feb738551f3a03d98aae
Nicole Casares <nicolecasares@gmail.com>
2020-09-07 21:44:30 -0700

    created hastag property

--- js/script.js
+++ js/script.js
@@ -17,22 +17,27 @@ let quotes = [
     'quote': 'I also say to my team: Do 10% of your job shittily. Its okay to do something shittily. Perfectionism prevents us from taking double steps in our career. We think we have to be perfect, but we dont.',
     'citation':'Brave Not Perfect',
     'year': '2019',
+    'hashtag':'#ilooklikeanengineer'
   },
   {
     'source': 'Sarah Friar, Chief Financial Officer at Square',
     'quote': 'A diversity of thought, perspective and culture is important in any field, not just engineering. Surround yourself with people who support you and get involved in [coding] programs.',
+    'hashtag':'#womensupportwomen'
   },
   {
     'source': 'Vanessa Hurst, Co-Founder of Girl Develop It',
     'quote': 'Feeling a little uncomfortable with your skills is a sign of learning, and continuous learning is what the tech industry thrives on! It’s important to seek out environments where you are supported, but where you have the chance to be uncomfortable and learn new things.',
+    'hashtag':'#womenempowerment'
   },
   {
     'source': 'Amber Gordon, Founder of Femsplain',
     'quote': 'Trust in yourself, believe that your voice matters, and know that your words are good enough.',
+    'hashtag':'#womeninbusiness'
   },
   {
     'source': 'Ara Katz, Chief Marketing Officer at Spring:',
     'quote': 'There is no recipe, there is no one way to do things — there is only your way. And if you can recognize that in yourself and accept and appreciate that in others, you can make magic.',
+    'hashtag':'#womeninbusiness'
   },
 
 ];
@@ -71,8 +76,9 @@ let printQuote = () => {
   let randomQuote = randomObject.quote;
   let randomSource = randomObject.source;
   let randomCitation = randomObject.citation;
+  let randomHashtag = randomObject.hashtag;
   let randomYear = randomObject.year;
-  let html = `<p class="quote"> ${randomQuote} </p>
+  let html = `<p class="hashtag">${randomHashtag}</p><p class="quote"> ${randomQuote} </p>
   <p class="source"> ${randomSource} `;
 
   if (randomCitation) {
@@ -101,3 +107,5 @@ let printQuote = () => {
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+setInterval(function(){ printQuote(); }, 10000);
